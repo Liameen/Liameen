9cf7f2ed8cd9f72aadbceb5a3a8de0f804662d77
Liameen <78691918+Liameen@users.noreply.github.com>
2021-07-15 17:44:39 +0100

    Add files via upload

---
+++ RPS.js
@@ -0,0 +1,19 @@
+
+
+ const button1 = document.getElementById("paperimg");
+
+button1.addEventListener('click', () => {
+    alert('you clicked paper');
+});
+
+const button2 = document.getElementById("rockimg");
+
+button1.addEventListener('click', () => {
+    alert('you clicked rock');
+});
+
+const button3 = document.getElementById("scissorsimg");
+
+button1.addEventListener('click', () => {
+    alert('you clicked scissors');
+});
